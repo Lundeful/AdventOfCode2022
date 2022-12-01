81963fb3ca290374cba4d6f0d9d3b623a4d4c9ae
Christoffer Lund <chrislund90@gmail.com>
2022-12-01 22:33:20 +0100

    Simplify day 1 - Did not need to print which elf had most calories

--- D1-1/Program.cs
+++ D1-1/Program.cs
@@ -1,17 +1,10 @@
 ﻿var caloriesPerElf = new List<int>();
 var currentElfCalorieCount = 0;
-var maxValue = 0;
-var maxValueIndex = 0;
 foreach (var line in File.ReadLines("./ElfCaloriesList.txt"))
 {
     if (string.IsNullOrEmpty(line))
     {
         caloriesPerElf.Add(currentElfCalorieCount);
-        if (currentElfCalorieCount > maxValue)
-        {
-            maxValue = currentElfCalorieCount;
-            maxValueIndex = caloriesPerElf.Count-1;
-        }
         currentElfCalorieCount = 0;
     }
     else
@@ -19,9 +12,7 @@ foreach (var line in File.ReadLines("./ElfCaloriesList.txt"))
         currentElfCalorieCount += int.Parse(line); 
     }
 }
-
-Console.WriteLine($"Elf number {maxValueIndex + 1} has the most calories with {caloriesPerElf[maxValueIndex]} calories");
-
 caloriesPerElf.Sort();
 caloriesPerElf.Reverse();
+Console.WriteLine($"The top elf has food worth {caloriesPerElf.First()} calories");
 Console.WriteLine($"The top three elves are carrying a total of {caloriesPerElf[0] + caloriesPerElf[1] + caloriesPerElf[2]} calories");

94543d921003d0f9eb004f93f06fb288b14f16f9
Christoffer Lund <chrislund90@gmail.com>
2022-12-06 10:58:06 +0100

    Turn logic into one-liner

--- D6/Program.cs
+++ D6/Program.cs
@@ -1,23 +1,4 @@
 ﻿var input = File.ReadLines("input.txt").First();
-const int numberOfUniqueCharacters = 14;
-var itemsBeforeMarker = FindMarker(input, numberOfUniqueCharacters);
-Console.WriteLine("Processed characters: " + (itemsBeforeMarker-1));
-
-static bool IsListUnique<T>(IReadOnlyCollection<T> list) => list.Distinct().Count() == list.Count;
-
-static int FindMarker(string text, int numberOfUniqueCharacters)
-{
-    var lastFour = text.Take(numberOfUniqueCharacters).ToList();
-
-    for (var i = 4; i < text.Length; i++)
-    {
-        if (IsListUnique(lastFour))
-            return i + 1;
-
-        lastFour = lastFour.Skip(1).ToList();
-        lastFour.Add(text[i]);
-    }
-
-    return -1;
-}
-
+const int numOfUniqueChars = 14;
+var result = input.TakeWhile((c, i) => i <= numOfUniqueChars || input.Skip(i-numOfUniqueChars).Take(numOfUniqueChars).Distinct().Count() < numOfUniqueChars).Count();
+Console.WriteLine(result);

9db2722e5ed59b50e8fa8028c3a0dd8ff348e694
Christoffer Lund <chrislund90@gmail.com>
2022-12-06 10:32:53 +0100

    Day 6 completed

---
+++ D6/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadLines("input.txt").First();
+const int numberOfUniqueCharacters = 14;
+var itemsBeforeMarker = FindMarker(input, numberOfUniqueCharacters);
+Console.WriteLine("Processed characters: " + (itemsBeforeMarker-1));
+
+static bool IsListUnique<T>(IReadOnlyCollection<T> list) => list.Distinct().Count() == list.Count;
+
+static int FindMarker(string text, int numberOfUniqueCharacters)
+{
+    var lastFour = text.Take(numberOfUniqueCharacters).ToList();
+
+    for (var i = 4; i < text.Length; i++)
+    {
+        if (IsListUnique(lastFour))
+            return i + 1;
+
+        lastFour = lastFour.Skip(1).ToList();
+        lastFour.Add(text[i]);
+    }
+
+    return -1;
+}
+

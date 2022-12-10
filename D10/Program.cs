9c5beb55c074e0af86cb03d4b98fe413821ac9bd
Christoffer Lund <chrislund90@gmail.com>
2022-12-10 15:27:52 +0100

    Day 9 Part 1 completed

--- D10/Program.cs
+++ D10/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
 
-Console.WriteLine("Hello, World!");
+var lines = File.ReadLines("input.txt").ToList();
+// lines = File.ReadLines("testInput.txt").ToList();
+
+var cycle = 0;
+var totalSignalStrength = 0;
+var registerValue = 1;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(" ");
+    if (parts[0] == "addx")
+    {
+        cycle++;
+        AddSignalStrength();
+        cycle++;
+        AddSignalStrength();
+        registerValue += int.Parse(parts[1]);
+    }
+    else
+    {
+        cycle++;
+        AddSignalStrength();
+    }
+}
+
+void AddSignalStrength()
+{
+    if (cycle is 20 or 60 or 100 or 140 or 180 or 220)
+    {
+        Console.WriteLine(" Cycle:  "  + cycle + ". Register value: " + registerValue + ". Total: " + registerValue * cycle);
+        totalSignalStrength += (registerValue * cycle);   
+    }
+}
+
+
+Console.WriteLine("Total signal strength: " + totalSignalStrength);

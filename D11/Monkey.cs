35532056b12ddf1fcd8458cb7b36e50ef5550f85
Christoffer Lund <chrislund90@gmail.com>
2022-12-11 10:25:43 +0100

    Day 11 completed

--- D11/Monkey.cs
+++ D11/Monkey.cs
@@ -1,12 +1,16 @@
+using System.Numerics;
+
+namespace D11;
+
 public class Monkey
 {
-    public int Id { get; set; }
-    public List<int> StartingItems { get; set; }
+    public long Id { get; set; }
+    public List<long> Items { get; set; }
     public string Operator { get; set; }
     public string Operand { get; set; }
-    public int Test { get; set; }
-    public int TrueDestination { get; set; }
-    public int FalseDestination { get; set; }
+    public long Test { get; set; }
+    public long TrueDestination { get; set; }
+    public long FalseDestination { get; set; }
 
-    public int ItemsInspected { get; set; } = 0;
+    public long ItemsInspected { get; set; } = 0;
 }
--- D11/Program.cs
+++ D11/Program.cs
@@ -1,47 +1,71 @@
-﻿using D11;
+﻿using System.Numerics;
+using D11;
 
-var lines = File.ReadLines("input.txt").ToList();
-// lines = File.ReadLines("testinput.txt").ToList();
+// var part1 = PlayTheFuckingGame(20);
+// Console.WriteLine("Monkey Business Part 1: " + part1);
 
-var monkeys = ParseInput(lines);
+var part2 = PlayTheFuckingGame(10000);
+Console.WriteLine("Monkey Business Part 2: " + part2);
 
-PlayTheFuckingGame();
-monkeys.Sort(new MonkeyComparer());
-Console.WriteLine("Monkey Business Part 1: " + monkeys[0].ItemsInspected * monkeys[1].ItemsInspected);
-
-void PlayTheFuckingGame()
+long PlayTheFuckingGame(long rounds)
 {
-    for (var i = 0; i < 20; i++)
+    var lines = File.ReadLines("input.txt").ToList();
+    // lines = File.ReadLines("testinput.txt").ToList();
+
+    var monkeys = ParseInput(lines);
+    long mod = monkeys.Select(m => m.Test).Aggregate((a, b) => a * b);
+
+    for (var i = 0; i < rounds; i++)
     {
+        if (i is (1 or 20) || i % 1000 == 0)
+        {
+            var a = monkeys[0].ItemsInspected;
+            var b = monkeys[1].ItemsInspected;
+            var c = monkeys[2].ItemsInspected;
+            var d = monkeys[3].ItemsInspected;
+            var e = 2;
+        }
+
         foreach (var monkey in monkeys)
         {
-            foreach (var monkeyStartingItem in monkey.StartingItems)
+            foreach (var item in monkey.Items)
             {
-                var myWorryLevel = monkeyStartingItem;
-
+                // var myWorryLevel = item;  // Part 1
+                var myWorryLevel = item % mod == 0 ? item : item % mod; // Apply relief for part 2 
+                
                 // Inspect item and apply worry level
-                var operand = monkey.Operand == "old" ? myWorryLevel : int.Parse(monkey.Operand);
-                if (monkey.Operator == "*")
-                    myWorryLevel *= operand;
-                else if (monkey.Operator == "+") myWorryLevel += operand;
+                var operand = monkey.Operand == "old" ? myWorryLevel : long.Parse(monkey.Operand);
 
-                // Apply relief
-                myWorryLevel /= 3;
+                switch (monkey.Operator)
+                {
+                    case "*":
+                        myWorryLevel *= operand;
+                        break;
+                    case "+":
+                        myWorryLevel += operand;
+                        break;
+                }
+
+                // Apply relief for part 1
+                // myWorryLevel /= 3;
 
                 // Apply test and throw
                 var testPassed = myWorryLevel % monkey.Test == 0;
                 var targetMonkey = monkeys.Find(m =>
                     m.Id == (testPassed ? monkey.TrueDestination : monkey.FalseDestination));
-                targetMonkey?.StartingItems.Add(myWorryLevel);
-
-                monkey.ItemsInspected++;
+                targetMonkey?.Items.Add(myWorryLevel);
             }
 
-            monkey.StartingItems = new();
+            monkey.ItemsInspected += monkey.Items.Count;
+            monkey.Items = new();
         }
     }
-}
 
+    monkeys.Sort(new MonkeyComparer());
+    var first = monkeys[0].ItemsInspected;
+    var second = monkeys[1].ItemsInspected;
+    return first * second;
+}
 
 List<Monkey> ParseInput(IReadOnlyList<string> input)
 {
@@ -49,29 +73,29 @@ List<Monkey> ParseInput(IReadOnlyList<string> input)
 
     for (var i = 0; i < input.Count;)
     {
-        var id = int.Parse(input[i].Split(" ")[1].Split(":")[0]);
+        var id = long.Parse(input[i].Split(" ")[1].Split(":")[0]);
         i++;
         
-        var startingItems = input[i].Replace("  Starting items: ", "").Split(",").Select(int.Parse).ToList();
+        var startingItems = input[i].Replace("  Starting items: ", "").Split(",").Select(long.Parse).ToList();
         i++;
         
         var operationType = input[i].Replace("  Operation: new = old ", "").Split(" ")[0];
         var operationNumber = input[i].Replace("  Operation: new = old ", "").Split(" ")[1];
         i++;
         
-        var test = int.Parse(input[i].Split(" ").Last());
+        var test = long.Parse(input[i].Split(" ").Last());
         i++;
         
-        var trueDest = int.Parse(input[i].Split(" ").Last());
+        var trueDest = long.Parse(input[i].Split(" ").Last());
         i++;
         
-        var falseDest = int.Parse(input[i].Split(" ").Last());
+        var falseDest = long.Parse(input[i].Split(" ").Last());
         i++;
 
         var monkey = new Monkey
         {
             Id = id,
-            StartingItems = startingItems,
+            Items = startingItems,
             Operator = operationType,
             Operand = operationNumber,
             Test = test,

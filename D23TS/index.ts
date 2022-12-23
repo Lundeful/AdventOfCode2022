3bcdc12aa5249024eb32581450c13612c3c4a7f2
Christoffer Lund <chrislund90@gmail.com>
2022-12-23 16:24:09 +0100

    day 23 part 1 cleanup - still doesnt work

--- D23TS/index.ts
+++ D23TS/index.ts
@@ -14,14 +14,12 @@ interface Coordinate {
 
 interface Elf {
     id: number;
-    initialPos: Coordinate;
     currentPos: Coordinate;
     nextPos: Coordinate;
 }
 
 const getElves = (lines: string[]): Elf[] => {
     const elves: Elf[] = [];
-
     let id = 1;
     for (let row = 0; row < lines.length; row++) {
         for (let col = 0; col < lines[row].length; col++) {
@@ -30,7 +28,6 @@ const getElves = (lines: string[]): Elf[] => {
                 const pos: Coordinate = { x: col, y: row };
                 const elf: Elf = {
                     id: id++,
-                    initialPos: pos,
                     currentPos: pos,
                     nextPos: pos,
                 };
@@ -41,7 +38,7 @@ const getElves = (lines: string[]): Elf[] => {
     return elves;
 };
 
-const pointsOverlap = (p1: Coordinate, p2: Coordinate) => p1.x == p2.x && p1.y == p2.y;
+const pointsOverlap = (p1: Coordinate, p2: Coordinate) => p1.x === p2.x && p1.y === p2.y;
 
 const isOverlapping = (positions: Coordinate[], pos: Coordinate): boolean => {
     return positions.some(p => pointsOverlap(p, pos));
@@ -75,31 +72,29 @@ const getNextPosition = (elves: Elf[], elf: Elf): Coordinate => {
     const southWestIsEmpty = !isOverlapping(currentElfPositions, southWest);
 
     if (northIsEmpty && northEastIsEmpty && eastIsEmpty && southEastIsEmpty && southIsEmpty && southWestIsEmpty && westIsEmpty && northWestIsEmpty) {
-        // console.log('All empty neighbours');
         return elf.currentPos;
     }
 
     let nextPosition: Coordinate | null = null;
 
-    for (let i = 0; i < directions.length && nextPosition == null; i++) {
+    for (let i = 0; i < directions.length && nextPosition === null; i++) {
         const dir = directions[i];
 
-        if (dir == 'N' && northIsEmpty && northEastIsEmpty && northWestIsEmpty) {
+        if (dir === 'N' && northIsEmpty && northEastIsEmpty && northWestIsEmpty) {
             nextPosition = north;
-        } else if (dir == 'S' && southIsEmpty && southEastIsEmpty && southWestIsEmpty) {
+        } else if (dir === 'S' && southIsEmpty && southEastIsEmpty && southWestIsEmpty) {
             nextPosition = south;
-        } else if (dir == 'W' && westIsEmpty && northWestIsEmpty && southWestIsEmpty) {
+        } else if (dir === 'W' && westIsEmpty && northWestIsEmpty && southWestIsEmpty) {
             nextPosition = west;
-        } else if (dir == 'E' && eastIsEmpty && northEastIsEmpty && southEastIsEmpty) {
+        } else if (dir === 'E' && eastIsEmpty && northEastIsEmpty && southEastIsEmpty) {
             nextPosition = east;
         }
     }
 
-    // console.log(nextPosition);
     return nextPosition ?? elf.currentPos;
 };
 
-const printGrid = (elves: Elf[], toFile: boolean) => {
+const printGrid = (elves: Elf[]) => {
     console.log('### GRID ###\n');
 
     const elfPositions = elves.map(e => e.currentPos);
@@ -129,43 +124,33 @@ const printGrid = (elves: Elf[], toFile: boolean) => {
     const padding = 0;
     for (let row = minY - padding; row <= maxY + padding; row++) {
         for (let col = minX - padding; col <= maxX + padding; col++) {
-            const elfIsPresent = elves.some(e => e.currentPos.x === col && e.currentPos.y === row);
+            const elfIsPresent = elves.some(e => pointsOverlap(e.currentPos, { x: col, y: row }));
             content += elfIsPresent ? '#' : '.';
         }
 
         content += '\n';
     }
 
-    if (toFile) {
-        fs.writeFile('output.txt', content, err => {
-            if (err) {
-                console.error(err);
-            }
-            // file written successfully
-        });
-    } else {
-        console.log(content);
-    }
+    console.log(content);
 };
 
 const getEmptyGroundTiles = (elves: Elf[]) => {
     for (let round = 0; round < rounds; round++) {
         // Calculate next move
         elves.forEach(elf => {
-            elf.nextPos = getNextPosition(elves, elf);
+            elf.nextPos = getNextPosition([...elves], { ...elf });
         });
 
         // Execute next move
         elves.forEach(elf => {
             const movesAreOverlapping = elves.some(e => e.id !== elf.id && pointsOverlap(e.nextPos, elf.nextPos));
             if (!movesAreOverlapping) {
-                elf.currentPos = elf.nextPos;
+                elf.currentPos = { ...elf.nextPos };
             }
         });
 
+        // Change directions for next round
         directions.push(directions.shift()!);
-
-        // if (elves.length < 50) printGrid(elves); // Don't bother printing large results
     }
 
     const minY = Math.min.apply(
@@ -188,29 +173,31 @@ const getEmptyGroundTiles = (elves: Elf[]) => {
         elves.map(e => e.currentPos.x)
     );
 
-    const width = maxX - minX;
-    const height = maxY - minY;
-    const numberOfTiles = (width + 1) * (height + 1);
+    const width = maxX - minX + 1;
+    const height = maxY - minY + 1;
+    const numberOfTiles = width * height;
     const numberOfElves = elves.length;
     const emptyTiles = numberOfTiles - numberOfElves;
 
+    console.log('Min X', minX, 'Max X', maxX);
+    console.log('Min Y', minY, 'Max Y', maxY);
     console.log('Width', width, 'Height', height);
     console.log('Tiles', numberOfTiles);
     console.log('Elves', numberOfElves);
     return emptyTiles;
 };
 
+console.log('Test');
 const testElves = getElves(testLines);
 const emptyTestTiles = getEmptyGroundTiles(testElves);
 const expectedResult = 110;
 const success = emptyTestTiles === expectedResult;
 console.log('Empty test tiles:', emptyTestTiles, 'Should be:', expectedResult, 'Success:', success);
 
-if (success) {
-    console.log();
-    const realElves = getElves(realLines);
-    const emptyRealTiles = getEmptyGroundTiles(realElves);
-    console.log('Empty real tiles', emptyRealTiles);
-    // 3709 - Wrong
-    printGrid(realElves, true);
-}
+console.log();
+console.log('Real');
+const realElves = getElves(realLines);
+const emptyRealTiles = getEmptyGroundTiles(realElves);
+console.log('Empty real tiles', emptyRealTiles);
+// 3709 - Wrong, too low
+// 3867 - Wrong
